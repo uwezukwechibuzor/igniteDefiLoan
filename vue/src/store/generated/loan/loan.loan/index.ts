b4216d100bd6b89b2e31526e884b624bb3ca3de5
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-07-16 13:19:06 +0100

    Add approve loan message

--- vue/src/store/generated/loan/loan.loan/index.ts
+++ vue/src/store/generated/loan/loan.loan/index.ts
@@ -214,6 +214,21 @@ export default {
 				}
 			}
 		},
+		async sendMsgApproveLoan({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgApproveLoan(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgApproveLoan:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgApproveLoan:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		
 		async MsgRequestLoan({ rootGetters }, { value }) {
 			try {
@@ -225,6 +240,19 @@ export default {
 					throw new Error('TxClient:MsgRequestLoan:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgRequestLoan:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgApproveLoan({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgApproveLoan(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgApproveLoan:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgApproveLoan:Create Could not create message: ' + e.message)
 				}
 			}
 		},
